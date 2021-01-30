ac61055d9b8ecff2d3d397b44004ea5266be12fd
bonnerky <kylebonner99@gmail.com>
2021-01-30 01:23:24 -0800

    added error unhandled and handled files

---
+++ Error_unhandled.py
@@ -0,0 +1,15 @@
+print ("This program tells you if a year is a leap year.")
+myyear = input("Enter a valid year")
+
+
+if (int(myyear) % 4) == 0: #if it is evenly distributed by 4
+    if (int(myyear) % 100) == 0: #if it is evenly distributed by 100
+        if (int(myyear) % 400) == 0: #if it is evenly distributed by 400
+            print("this is a leap year!")
+        else:
+            print("year is not a leap year")
+    else:
+        print("year is a leap year!")
+else:
+    print ("year is not a leap year")
+
